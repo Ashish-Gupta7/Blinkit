401ada7668667ef70fd8b1836c8da0112e5f302a
Ashish-Gupta7 <ash300mr@gmail.com>
2024-09-19 17:15:31 +0530

    mongodb connection done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+require("./config/mongoose");
+const dbgr = require("debug")("development:App");
 
 const indexRouter = require("./routes/index");
 
@@ -7,5 +10,5 @@ app.get("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:3000`);
+  dbgr(`Server is running on http://localhost:3000`);
 });
